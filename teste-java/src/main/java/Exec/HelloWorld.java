f7483930a0cb76b4672cd25873a4a3f489cad260
eric-viezzer <eric.lessa@bandtec.com.br>
2021-05-20 21:35:00 -0300

    a

---
+++ teste-java/src/main/java/Exec/HelloWorld.java
@@ -0,0 +1,7 @@
+package Exec;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Olá sou a prova que seu java \n "
+                         + "esta pronto pra uso :)");
+    }
+}
